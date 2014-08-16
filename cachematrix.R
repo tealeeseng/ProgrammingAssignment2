68fd4f86e334c0cdbfdcd917eeb429349926b7a6
Lee Seng <tealeeseng@gmail.com>
2014-08-16 23:49:27 +0800

    verified working caching matrix solver

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        solved <-NULL
+        
+        # remove set opration. not required.
+        get <-function() x
+        
+        # caching result.
+        setsolved <- function(solve) solved <<- solve
+        # return cached result.
+        getsolved <- function() solved
+        
+        # return functions as list.
+        list( get = get, setsolved=setsolved, getsolved=getsolved)
 
 }
 
@@ -12,4 +24,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    solved <-x$getsolved()
+    # x shall be a global variable.
+    if(!is.null(solved)){
+        # second invocation of the same global variable x will reach here.
+        message("Used cached result.")
+        solved
+    }
+    
+    #cache missed come here and all the hardwork starting here.
+    raw <- x$get()
+    solved <- solve(raw,...)
+    
+    # saving result to cache
+    x$setsolved(solved)
+    return(solved)
+    
 }
